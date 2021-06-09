27ec544e0425adca1299a659371c49b554983ffd
Valter-Hashimoto <valterhashimoto@gmail.com>
2021-06-09 16:59:03 -0300

    Consulta simples com query methods

--- src/main/java/com/educandoweb/course/resources/PostResource.java
+++ src/main/java/com/educandoweb/course/resources/PostResource.java
@@ -1,15 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.domain.Post;
-import com.educandoweb.course.domain.User;
-import com.educandoweb.course.dto.UserDTO;
+import com.educandoweb.course.resources.util.URL;
 import com.educandoweb.course.services.PostService;
 
 @RestController
@@ -19,8 +21,6 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 
-	
-
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
@@ -28,7 +28,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
+
+	@RequestMapping(value = "/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
-
-
-	
